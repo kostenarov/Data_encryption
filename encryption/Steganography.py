753edcb72ddeb63eed548473005b6e7e24521297
emsiish <61331069+emsiish@users.noreply.github.com>
2023-03-09 23:08:49 +0200

    new encode function

--- encryption/Steganography.py
+++ encryption/Steganography.py
@@ -2,16 +2,33 @@ import numpy as np
 from PIL import Image
 from stegano import lsb
 
-def encode(message, src=r"C:\Users\petoc\PycharmProjects\Data_encryption\demo\mountainLake.png"):
-    # open image and convert from rgb to rgba and get size
+def encode(message, src):
     img = Image.open(src)
+    img = img.convert("RGBA")
+    width, height = img.size
 
-    # Encode the message into the image using LSB
-    enc_img = lsb.hide(img, message)
-
-    # Save the encoded image
-    enc_img.save(r"C:\Users\petoc\PycharmProjects\Data_encryption\demo\test.png")
-
+    array = np.array(list(img.getdata()))
+    mode = 4
+    pixels = array.size // mode
+    if len(message) > pixels // 4:
+        raise ValueError("Message is too long for the image")
+    message += '\0'
+    binary_message = ''.join([format(ord(i), "08b") for i in message])
+    required_pixels = len(binary_message)
+    index = 0
+    for p in range(pixels):
+        for q in range(0, 4):
+            if index < required_pixels:
+                binary_pixel = format(array[p][q], "08b")
+                binary_pixel = binary_pixel[:-1] + binary_message[index]
+                array[p][q] = int(binary_pixel, 2)
+                index += 1
+            else:
+                break
+    array = array.reshape(height, width, mode)
+    enc_img = Image.fromarray(array.astype('uint8'), img.mode)
+    enc_img.save("encoded.png")
+    enc_img.show()
 
 def decode(src=r"C:\Users\petoc\PycharmProjects\Data_encryption\demo\test.png"):
     img = Image.open(src, 'r')
